c476e061146e976897639f81a76d227967296414
nguyendangbanghn93 <55304669+nguyendangbanghn93@users.noreply.github.com>
2020-04-05 20:28:30 +0700

    upp bài

---
+++ adf2test/src/main/java/adf2/helper/ConnectionHelper.java
@@ -0,0 +1,22 @@
+package adf2.helper;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectionHelper {
+
+    private static final String DATABASE_URL = "jdbc:mysql://localhost/account?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=Asia/Bangkok&characterEncoding=utf-8";
+    private static final String DATABASE_USER = "root";
+    private static final String DATABASE_PWD = "";
+    private static Connection connection; // ban đầu bằng null.
+
+    public static Connection getConnection() throws SQLException {
+        if (connection == null) {
+            System.out.println("Khởi tạo kết nối mới đến db.");
+            connection = DriverManager
+                    .getConnection(DATABASE_URL, DATABASE_USER, DATABASE_PWD);
+        }
+        return connection;
+    }
+}
